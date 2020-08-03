240cce08ad4d308c1bb93a2b9183978aa5cd5cab
sumugit <diddy2983@yahoo.co.jp>
2020-08-03 10:58:04 +0900

    メッセージ完成

--- chat.php
+++ chat.php
@@ -5,14 +5,14 @@ session_regenerate_id(true);
 //変数がセットされているか
 if (isset($_SESSION['login']) == false) {
     print 'ログインされていません。<br>';
-    print '<a href="login.html">ログイン画面へ</a><br>';
+    print '<a href="login.php">ログイン画面へ</a><br>';
     print '<br>';
     exit();
 } else {
-    print 'ようこそ';
+    print '<p id ="login">';
     print $_SESSION['nickname'] . '様-';
-    print '<a href="logout.php">ログアウト</a><br>';
-    print '<br>';
+    print '<a href="logout.php"><b>ログアウト</b></a>';
+    print '<br><p>';
 }
 ?>
 
@@ -21,126 +21,156 @@ if (isset($_SESSION['login']) == false) {
 
 <head>
     <meta charset="utf-8" />
-    <title>メッセージ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Neucha' rel='stylesheet' type='text/css'>
+    <link href="css/style3.css" rel="stylesheet">
     <link rel="stylesheet" href="css/chat.css">
+    <link href="css/chatStyle.css" rel="stylesheet">
+    <title>メッセージ</title>
 </head>
 
 <body>
-    <?php
-    //URLパラメータからid取得
-    $id = htmlspecialchars($_GET['id']);
-    if (is_file("./csv/profile.csv")) { //登録ファイルが存在するか
-        if (is_readable("./csv/profile.csv")) { //登録ファイルを読み込めるか
-            $fp = fopen("./csv/profile.csv", "r");
-            flock($fp, LOCK_SH);
-            //idが相手のアカウントと一致するまで一行ずつ取り出す
-            while (!feof($fp)) {
-                $content = fgetcsv($fp);
-                //閲覧しているユーザーの情報を取得
-                if ($content[0] == $id) {
-                    break;
+    <div id="contenar">
+        <div id="field">
+            <div id="home" class="top-image-area">
+                <a href="index.php"><img class="logo" src="siteimages/header2.png" alt="マッチングナビロゴ"></a>
+                <header class="page-header wrapper">
+                    <nav>
+                        <ul class="main-nav">
+                            <li><a href="index.php" class="btn4">ホーム</a></li>
+                            <li><a href="search.php" class="btn4">プロフ検索</a></li>
+                            <li><a href="bulletinInfo.php" class="btn4">掲示板</a></li>
+                            <li><a href="chatInfo.php" class="btn4">メッセージ</a></li>
+                            <li><a href="myPage.php" class="btn4">マイページ</a></li>
+                            <li><a href="contact.html" class="btn4">お問い合わせ</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <?php
+                //URLパラメータからid取得
+                $id = htmlspecialchars($_GET['id']);
+                if (is_file("./csv/profile.csv")) { //登録ファイルが存在するか
+                    if (is_readable("./csv/profile.csv")) { //登録ファイルを読み込めるか
+                        $fp = fopen("./csv/profile.csv", "r");
+                        flock($fp, LOCK_SH);
+                        //idが相手のアカウントと一致するまで一行ずつ取り出す
+                        while (!feof($fp)) {
+                            $content = fgetcsv($fp);
+                            //閲覧しているユーザーの情報を取得
+                            if ($content[0] == $id) {
+                                break;
+                            }
+                        }
+                        //ファイルを閉じる
+                        flock($fp, LOCK_UN);
+                        fclose($fp);
+                    } else {
+                        echo "ファイルが開けません。";
+                        exit();
+                    }
+                } else {
+                    echo "ファイルがありません。";
+                    exit();
                 }
-            }
-            //ファイルを閉じる
-            flock($fp, LOCK_UN);
-            fclose($fp);
-        } else {
-            echo "ファイルが開けません。";
-            exit();
-        }
-    } else {
-        echo "ファイルがありません。";
-        exit();
-    }
-    ?>
-    <div id="container">
-        <?php print "<h1>$content[3]さんとのメッセージ</h1>"; ?>
-        <div id="talkField">
-            <!--resultにチャットのログが入る-->
-            <div id="result"></div>
-            <br class="clear_balloon" />
-            <!--チャットの末尾記録用-->
-            <div id="end"></div>
-        </div>
-        <!--入力部分-->
-        <div id="inputField">
-            <p>
-                <?php echo 'ニックネーム: ' . $_SESSION['nickname'] ?><br>
-                メッセージ: <input type="text" name="message" id="message">
-                <input type="button" id="greet" value="送信" onclick="addText();">
-            </p>
+                ?>
+                <div id="container">
+                    <?php print "<h1>$content[3]</h1>"; ?>
+                    <div id="talkField">
+                        <!--resultにチャットのログが入る-->
+                        <div id="result"></div>
+                        <br class="clear_balloon" />
+                        <!--チャットの末尾記録用-->
+                        <div id="end"></div>
+                    </div>
+                    <!--入力部分-->
+                    <div id="inputField">
+                        <p>
+                            <?php echo 'ニックネーム: ' . $_SESSION['nickname'] ?><br>
+                            <textarea name="message" id="message" placeholder="メッセージ本文" required></textarea><br>
+                            <input type="button" id="greet" value="送信" class="submit" onclick="addText();">
+                        </p>
+                    </div>
+                </div>
+                <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+                <script type="text/javascript">
+                    window.onload = function() {
+                        loadLog();
+                    }
+
+                    function addText() {
+                        // 入力されたID値を取得
+                        $(function() {
+                            if (!$('#message').val()) return;
+                            //Ajax通信(いいね押して別のページに遷移するのは不便だから)
+                            $.ajax({
+                                type: "POST",
+                                url: "chatLog.php",
+                                data: {
+                                    "mine": <?php echo $_SESSION['id']; ?>,
+                                    "opponent": <?php echo $id; ?>,
+                                    "nickname": <?php echo "'$_SESSION[nickname]'"; ?>,
+                                    "message": $('#message').val(),
+                                    "mode": "0" // 書き込み
+                                },
+                                dataType: "text"
+                            }).done(function(data) {
+                                $('#result').html(data);
+                            }).fail(function(XMLHttpRequest, textStatus, error) {
+                                alert(error);
+                            });
+                        });
+                        loadLog();
+                    }
+
+                    // ログをロードする
+                    function loadLog() {
+                        $.ajax({
+                            type: "POST",
+                            url: "chatLog.php",
+                            data: {
+                                "mine": <?php echo $_SESSION['id']; ?>,
+                                "opponent": <?php echo $id; ?>,
+                                "mode": "1" // 書き込み
+                            },
+                            dataType: "text"
+                        }).done(function(data) {
+                            $('#result').html(data);
+                            scTarget();
+                        });
+                    }
+
+                    // 一定間隔でログをリロードする
+                    /*function logAll(){
+                    	setTimeout(function(){
+                    		loadLog();
+                    		logAll();
+                    	},5000); //リロード時間はここで調整
+                    }*/
+
+                    /*
+                     * 画面を最下部へ移動させる
+                     */
+                    function scTarget() {
+                        //end idの位置
+                        var pos = $("#end").offset().top;
+                        $("#talkField").animate({
+                            scrollTop: pos
+                        }, 1000, "swing"); //swingで1秒で画面下に遷移するアニメーション
+                        return false;
+                    }
+                </script>
+            </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript">
-        window.onload = function() {
-            loadLog();
-        }
-
-        function addText() {
-            // 入力されたID値を取得
-            $(function() {
-                if (!$('#message').val()) return;
-                //Ajax通信(いいね押して別のページに遷移するのは不便だから)
-                $.ajax({
-                    type: "POST",
-                    url: "chatLog.php",
-                    data: {
-                        "mine": <?php echo $_SESSION['id']; ?>,
-                        "opponent": <?php echo $id; ?>,
-                        "nickname": <?php echo "'$_SESSION[nickname]'"; ?>,
-                        "message": $('#message').val(),
-                        "mode": "0" // 書き込み
-                    },
-                    dataType: "text"
-                }).done(function(data) {
-                    $('#result').html(data);
-                }).fail(function(XMLHttpRequest, textStatus, error) {
-                    alert(error);
-                });
-            });
-            loadLog();
-        }
-
-        // ログをロードする
-        function loadLog() {
-            $.ajax({
-                type: "POST",
-                url: "chatLog.php",
-                data: {
-                    "mine": <?php echo $_SESSION['id']; ?>,
-                    "opponent": <?php echo $id; ?>,
-                    "mode": "1" // 書き込み
-                },
-                dataType: "text"
-            }).done(function(data) {
-                $('#result').html(data);
-                scTarget();
-            });
-        }
-
-        // 一定間隔でログをリロードする
-        /*function logAll(){
-        	setTimeout(function(){
-        		loadLog();
-        		logAll();
-        	},5000); //リロード時間はここで調整
-        }*/
-
-        /*
-         * 画面を最下部へ移動させる
-         */
-        function scTarget() {
-            //end idの位置
-            var pos = $("#end").offset().top;
-            $("#talkField").animate({
-                scrollTop: pos
-            }, 1000, "swing"); //swingで1秒で画面下に遷移するアニメーション
-            return false;
-        }
-    </script>
 </body>
-<footer>
-    <p><small>&copy; マッチングナビ 2020 仮</small></p>
-</footer>
+<div class="footer-wrapper">
+    <footer>
+        <p><small>&copy; マッチングナビ 2020</small></p>
+    </footer>
+</div>
+
 </html>
--- chatInfo.php
+++ chatInfo.php
@@ -5,14 +5,14 @@ session_regenerate_id(true);
 //変数がセットされているか
 if (isset($_SESSION['login']) == false) {
     print 'ログインされていません。<br>';
-    print '<a href="login.html">ログイン画面へ</a><br>';
+    print '<a href="login.php">ログイン画面へ</a><br>';
     print '<br>';
     exit();
 } else {
-    print 'ようこそ';
+    print '<p id ="login">';
     print $_SESSION['nickname'] . '様-';
-    print '<a href="logout.php">ログアウト</a><br>';
-    print '<br>';
+    print '<a href="logout.php"><b>ログアウト</b></a>';
+    print '<br><p>';
 }
 ?>
 
@@ -23,75 +23,98 @@ if (isset($_SESSION['login']) == false) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Neucha' rel='stylesheet' type='text/css'>
     <link href="css/linestyle.css" rel="stylesheet">
+    <link href="css/style3.css" rel="stylesheet">
+    <link href="css/bulletinButton.css" rel="stylesheet">
     <title>メッセージ履歴</title>
 </head>
 
 <body>
-    <div class="container">
-        <?php
-        //メッセージの履歴があるかどうか
-        $flag = false;
-        //メッセージの確認
-        if (is_file("./csv/chatLog.csv")) { //ファイルが存在するか
-            if (is_readable("./csv/chatLog.csv")) { //ファイルを読み込めるか
-                $fp1 = fopen("./csv/chatLog.csv", "r");
-                flock($fp1, LOCK_SH);
-                while (!feof($fp1)) {
-                    $content = fgetcsv($fp1);
-                    //自分のidの検索
-                    if ($content[0] == $_SESSION['id']) {
-                        $flag = true;
-                        if (is_file("./csv/profile.csv")) { //ファイルが存在するか
-                            if (is_readable("./csv/profile.csv")) { //ファイルを読み込めるか
-                                $fp2 = fopen("./csv/profile.csv", "r");
-                                flock($fp2, LOCK_SH);
-                                while (!feof($fp2)) {
-                                    $profile = fgetcsv($fp2);
-                                    //送った相手のid検索
-                                    if ($content[1] == $profile[0]) {
-                                        print '<div class="item">';
-                                        //URLパラメータ生成
-                                        print '<a href="chat.php?id=' . $profile[0] . '"><img src = ' . $profile[1] . ' alt=""></a>';
-                                        print '<p>' . $profile[2] . '</p>';
-                                        print '<input type="button" value="プロフィール" onclick="location.href=\'userPlofile.php?id=' . $profile[0] . '\'">';
-                                        print '</div>';
-                                        break;
+    <div id="contenar">
+        <div id="field">
+            <div id="home" class="top-image-area">
+                <a href="index.php"><img class="logo" src="siteimages/header2.png" alt="マッチングナビロゴ"></a>
+                <header class="page-header wrapper">
+                    <nav>
+                        <ul class="main-nav">
+                            <li><a href="index.php" class="btn4">ホーム</a></li>
+                            <li><a href="search.php" class="btn4">プロフ検索</a></li>
+                            <li><a href="bulletinInfo.php" class="btn4">掲示板</a></li>
+                            <li><a href="chatInfo.php" class="btn4">メッセージ</a></li>
+                            <li><a href="myPage.php" class="btn4">マイページ</a></li>
+                            <li><a href="contact.html" class="btn4">お問い合わせ</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <div class="container">
+                    <?php
+                    //メッセージの履歴があるかどうか
+                    $flag = false;
+                    //メッセージの確認
+                    if (is_file("./csv/chatLog.csv")) { //ファイルが存在するか
+                        if (is_readable("./csv/chatLog.csv")) { //ファイルを読み込めるか
+                            $fp1 = fopen("./csv/chatLog.csv", "r");
+                            flock($fp1, LOCK_SH);
+                            while (!feof($fp1)) {
+                                $content = fgetcsv($fp1);
+                                //自分のidの検索
+                                if ($content[0] == $_SESSION['id']) {
+                                    $flag = true;
+                                    if (is_file("./csv/profile.csv")) { //ファイルが存在するか
+                                        if (is_readable("./csv/profile.csv")) { //ファイルを読み込めるか
+                                            $fp2 = fopen("./csv/profile.csv", "r");
+                                            flock($fp2, LOCK_SH);
+                                            while (!feof($fp2)) {
+                                                $profile = fgetcsv($fp2);
+                                                //送った相手のid検索
+                                                if ($content[1] == $profile[0]) {
+                                                    //URLパラメータ生成
+                                                    print '<a href="chat.php?id=' . $profile[0] . '"><div class="item"><img src = ' . $profile[1] . ' align="left" width="128" height="128" alt=""><p>　' . $profile[4] . ' ' . $profile[5] . ' </p><p id="big">　' . $profile[3] . ' </p><hr  style="border:1px dashed #000000;"><p>　' . $profile[2] . ' </p></div></a>';
+                                                    print '<a href="./userPlofile.php?id=' . $profile[0] . '" class="btn btn--yellow btn--cubic">プロフィール確認</a>';
+                                                    break;
+                                                }
+                                            }
+                                            flock($fp2, LOCK_UN);
+                                            fclose($fp2);
+                                        } else {
+                                            echo "ファイルが開けません。";
+                                            exit();
+                                        }
+                                    } else {
+                                        echo "ファイルがありません。";
                                     }
                                 }
-                                flock($fp2, LOCK_UN);
-                                fclose($fp2);
-                            } else {
-                                echo "ファイルが開けません。";
-                                exit();
+                            }
+                            flock($fp1, LOCK_UN);
+                            fclose($fp1);
+                            if ($flag == false) {
+                                print "メッセージの履歴がありません。<br><br>";
+                                print '<form>';
+                                print '<input type="button" onclick="history.back()" value="戻る">';
+                                print '</form>';
                             }
                         } else {
-                            echo "ファイルがありません。";
+                            echo "ファイルが開けません。";
+                            exit();
                         }
+                    } else {
+                        echo "ファイルがありません。";
+                        exit();
                     }
-                }
-                flock($fp1, LOCK_UN);
-                fclose($fp1);
-                if ($flag == false) {
-                    print "メッセージの履歴がありません。<br><br>";
-                    print '<form>';
-                    print '<input type="button" onclick="history.back()" value="戻る">';
-                    print '</form>';
-                }
-            } else {
-                echo "ファイルが開けません。";
-                exit();
-            }
-        } else {
-            echo "ファイルがありません。";
-            exit();
-        }
-        ?>
+                    ?>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
-<footer>
-    <p><small>&copy; マッチングナビ 2020 仮</small></p>
-</footer>
+<div class="footer-wrapper">
+    <footer>
+        <p><small>&copy; マッチングナビ 2020</small></p>
+    </footer>
+</div>
 
 </html>
