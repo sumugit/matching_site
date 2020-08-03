049e4b2d4c3a16379a1fcd7176dd5c134bf0c4b9
sumugit <diddy2983@yahoo.co.jp>
2020-08-03 14:35:58 +0900

    2周目:indexのフレーム編集

--- index.php
+++ index.php
@@ -8,12 +8,13 @@ if (isset($_SESSION['login']) == false) {
     print '<b><a href="login.php">ログイン</a></b>';
     print '<br><p>';
 } else {
-    print '<p id ="login">ようこそ';
+    print '<p id ="login">';
     print $_SESSION['nickname'] . '様-';
     print '<a href="logout.php"><b>ログアウト</b></a>';
     print '<br><p>';
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -23,6 +24,7 @@ if (isset($_SESSION['login']) == false) {
     <meta name="description" content="異性とのマッチングを促すサイトです。">
 
     <!--CSS-->
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
     <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
@@ -32,76 +34,104 @@ if (isset($_SESSION['login']) == false) {
 </head>
 
 <body>
-    <div id="contenar">
-        <div id="field">
-            <div id="home" class="top-image-area">
-                <a href="index.php"><img class="logo" src="siteimages/header2.png" alt="マッチングナビロゴ"></a>
-                <header class="page-header wrapper">
-                    <nav>
-                        <ul class="main-nav">
-                            <li><a href="index.php" class="btn4">ホーム</a></li>
-                            <li><a href="search.php" class="btn4">プロフ検索</a></li>
-                            <li><a href="bulletinInfo.php" class="btn4">掲示板</a></li>
-                            <li><a href="chatInfo.php" class="btn4">メッセージ</a></li>
-                            <li><a href="myPage.php" class="btn4">マイページ</a></li>
-                            <li><a href="contact.html" class="btn4">お問い合わせ</a></li>
-                        </ul>
-                    </nav>
-                </header>
-                <br><br><br>
-                <a href="./enrollInfo.php" class="btn btn-c btn--green btn--cubic">新規会員登録</a>
-                <a href="./login.php" class="btn2 btn-c btn--green btn--cubic">　ログイン　</a>
-                <!-- 画面上部の帯 -->
-                <div id="top_belt"></div>
-                <!-- スライド表示枠 -->
-                <div id="stage">
-                    <!-- スライド切換えボタン用ラジオボタン -->
-                    <input type="radio" id="r1" name="gal">
-                    <input type="radio" id="r2" name="gal">
-                    <input type="radio" id="r3" name="gal">
-                    <!-- 送りボタン用ラジオボタン -->
-                    <input type="radio" id="back1" name="gal"><input type="radio" id="next1" name="gal">
-                    <input type="radio" id="back2" name="gal"><input type="radio" id="next2" name="gal">
-                    <input type="radio" id="back3" name="gal"><input type="radio" id="next3" name="gal">
-                    <!-- 現スライド標示ボタンのラジオボタンとの関連付け -->
-                    <label for="r1" id="lb1" class="circ"><img src="siteimages/2.png"></label>
-                    <label for="r2" id="lb2" class="circ"><img src="siteimages/2.png"></label>
-                    <label for="r3" id="lb3" class="circ"><img src="siteimages/2.png"></label>
-                    <!-- スライド群 -->
-                    <div id="photos">
-                        <!-- スライド1 -->
-                        <div id="photo1" class="pic">
-                            <!-- スライド写真と現スライド標示ボタン -->
-                            <img src="siteimages/1.jpeg"><img src="siteimages/1.png">
-                            <!-- 送りボタンの表示とラジオボタンとの関連付け -->
-                            <label for="back1"><span class="pb">＜</span></label>
-                            <label for="next1"><span class="nb">＞</span></label>
+    <header>
+        <h1>素敵な出会いをここで</h1>
+        <!--<p>マッチングナビはあなたに相性の良い異性とのマッチングを促します。</p>-->
+    </header>
+    <div class="wrapper">
+        <div class="left-column">
+            <!--左サイドバー-->
+            <p>あああああああああああああ</p>
+            <p>あああああああああああああ</p>
+            <p>あああああああああああああ</p>
+            <p>あああああああああああああ</p>
+        </div>
+        <div class="right-column">
+            <div id="contenar">
+                <div id="field">
+                    <div id="home" class="top-image-area">
+                        <a href="index.php"><img class="logo" src="siteimages/header2.png" alt="マッチングナビロゴ"></a>
+                        <header class="page-header wrapper">
+                            <nav>
+                                <ul class="main-nav">
+                                    <li><a href="index.php" class="btn4"><i class="fas fa-home"></i>ホーム</a></li>
+                                    <li><a href="search.php" class="btn4"><i class="fas fa-search"></i>プロフ検索</a></li>
+                                    <li><a href="bulletinInfo.php" class="btn4"><i class="fas fa-clipboard"></i>掲示板</a></li>
+                                    <li><a href="chatInfo.php" class="btn4"><i class="fas fa-comment-dots"></i>メッセージ</a></li>
+                                    <li><a href="myPage.php" class="btn4"><i class="fas fa-address-card"></i>マイページ</a></li>
+                                    <li><a href="contact.php" class="btn4"><i class="fas fa-phone"></i>お問い合わせ</a></li>
+                                </ul>
+                            </nav>
+                        </header>
+                        <br><br><br>
+                        <a href="./enrollInfo.php" class="btn btn-c btn--green btn--cubic">新規会員登録</a>
+                        <a href="./login.php" class="btn2 btn-c btn--green btn--cubic">　ログイン　</a>
+                        <!-- 画面上部の帯 -->
+                        <div id="top_belt"></div>
+                        <!-- スライド表示枠 -->
+                        <div id="stage">
+                            <!-- スライド切換えボタン用ラジオボタン -->
+                            <input type="radio" id="r1" name="gal">
+                            <input type="radio" id="r2" name="gal">
+                            <input type="radio" id="r3" name="gal">
+                            <!-- 送りボタン用ラジオボタン -->
+                            <input type="radio" id="back1" name="gal"><input type="radio" id="next1" name="gal">
+                            <input type="radio" id="back2" name="gal"><input type="radio" id="next2" name="gal">
+                            <input type="radio" id="back3" name="gal"><input type="radio" id="next3" name="gal">
+                            <!-- 現スライド標示ボタンのラジオボタンとの関連付け -->
+                            <label for="r1" id="lb1" class="circ"><img src="siteimages/2.png"></label>
+                            <label for="r2" id="lb2" class="circ"><img src="siteimages/2.png"></label>
+                            <label for="r3" id="lb3" class="circ"><img src="siteimages/2.png"></label>
+                            <!-- スライド群 -->
+                            <div id="photos">
+                                <!-- スライド1 -->
+                                <div id="photo1" class="pic">
+                                    <!-- スライド写真と現スライド標示ボタン -->
+                                    <img src="siteimages/1.jpeg"><img src="siteimages/1.png">
+                                    <!-- 送りボタンの表示とラジオボタンとの関連付け -->
+                                    <label for="back1"><span class="pb">＜</span></label>
+                                    <label for="next1"><span class="nb">＞</span></label>
+                                </div>
+                                <div id="photo2" class="pic">
+                                    <img src="siteimages/2.jpg"><img src="siteimages/1.png">
+                                    <label for="back2"><span class="pb">＜</span></label>
+                                    <label for="next2"><span class="nb">＞</span></label>
+                                </div>
+                                <div id="photo3" class="pic">
+                                    <img src="siteimages/3.jpg"><img src="siteimages/1.png">
+                                    <label for="back3"><span class="pb">＜</span></label>
+                                    <label for="next3"><span class="nb">＞</span></label>
+                                </div>
+                            </div>
+                            <!-- stageの高さの確保 -->
+                            <div style="padding:28%;"></div>
                         </div>
-                        <div id="photo2" class="pic">
-                            <img src="siteimages/2.jpg"><img src="siteimages/1.png">
-                            <label for="back2"><span class="pb">＜</span></label>
-                            <label for="next2"><span class="nb">＞</span></label>
-                        </div>
-                        <div id="photo3" class="pic">
-                            <img src="siteimages/3.jpg"><img src="siteimages/1.png">
-                            <label for="back3"><span class="pb">＜</span></label>
-                            <label for="next3"><span class="nb">＞</span></label>
-                        </div>
-                    </div>
-                    <!-- stageの高さの確保 -->
-                    <div style="padding:28%;"></div>
-                </div>
 
-                <div class="home-content wrapper">
-                    <!--<h2 class="page-title">素敵な出会いをここで</h2>
+                        <div class="home-content wrapper">
+                            <!--<h2 class="page-title">素敵な出会いをここで</h2>
             <p>マッチングナビはあなたに相性の良い異性とのマッチングを促します。</p>-->
-                </div><!-- /.home-content-->
-            </div><!-- /#home -->
+                        </div><!-- /.home-content-->
+                    </div><!-- /#home -->
+                </div>
+            </div>
         </div>
     </div>
 </body>
 <div class="footer-wrapper">
     <footer>
+        <ul class="footer-menu">
+            <li><a href="index.php">ホーム</a></li>
+            <li>|</li>
+            <li><a href="search.php">プロフ検索</a></li>
+            <li>|</li>
+            <li><a href="bulletinInfo.php">掲示板</a></li>
+            <li>|</li>
+            <li><a href="chatInfo.php">メッセージ</a></li>
+            <li>|</li>
+            <li><a href="myPage.php">マイページ</a></li>
+            <li>|</li>
+            <li><a href="contact.php">お問い合わせ</a></li>
+        </ul>
         <p><small>&copy; マッチングナビ 2020</small></p>
     </footer>
 </div>
--- myPage.php
+++ myPage.php
@@ -31,32 +31,79 @@ if (isset($_SESSION['login']) == false) {
 
 <body>
     <div id="contenar">
-        <div id="field">
-            <div id="home" class="top-image-area">
-                <a href="index.php"><img class="logo" src="siteimages/header2.png" alt="マッチングナビロゴ"></a>
-                <header class="page-header wrapper">
-                    <nav>
-                        <ul class="main-nav">
-                            <li><a href="index.php" class="btn4">ホーム</a></li>
-                            <li><a href="search.php" class="btn4">プロフ検索</a></li>
-                            <li><a href="bulletinInfo.php" class="btn4">掲示板</a></li>
-                            <li><a href="chatInfo.php" class="btn4">メッセージ</a></li>
-                            <li><a href="myPage.php" class="btn4">マイページ</a></li>
-                            <li><a href="contact.html" class="btn4">お問い合わせ</a></li>
-                        </ul>
-                    </nav>
-                </header>
-                <div id="pad"></div>
-                <div class="container">
-                    <a href="./profile.php"><div class="item"><p>プロフィール</p></br><img src="./siteimages/profile.png" width="110" height="110" alt="アイコン"></div></a>
-                    <a href="./loginInfo.php"><div class="item"><p>登録情報</p><br><img src="./siteimages/user.jpg" width="120" height="120" alt="アイコン"></div></a>
-                    <a href="./confirmFootPrint.php"><div class="item"><p>足あと</p><img src="./siteimages/trace.png" width="156" height="156" alt="アイコン"></div></a>
-                    <a href="./confirmGood.php"><div class="item"><p>あなたへのいいね</p></br><img src="./siteimages/good.jpg" width="120" height="120" alt="アイコン"></div></a>
-                    <a href="./confirmLove.php"><div class="item"><p>あなたへのタイプ</p></br><img src="./siteimages/heart.png" width="156" height="120" alt="アイコン"></div></a>
-                    <a href="./confirmMyGood.php"><div class="item"><p>いいねした相手</p></br><img src="./siteimages/putGood.jpg" width="156" height="120" alt="アイコン"></div></a>
-                    <a href="./confirmMyLove.php"><div class="item"><p>タイプした相手</p></br><img src="./siteimages/putLove.jpg" width="156" height="120" alt="アイコン"></div></a>
-                    <a href="./info.php"><div class="item"><p>お知らせ</p></br><img src="./siteimages/info.png" width="130" height="130" alt="アイコン"></div></a>
-                    <a href="./news.php"><div class="item"><p>最新情報</p></br></br><img src="./siteimages/news.png" width="100" height="100" alt="アイコン"></div></a>
+        <div class="wrapper">
+            <div class="left-column">
+                <!--左サイドバー-->
+                <p>あああああああああああああ</p>
+                <p>あああああああああああああ</p>
+                <p>あああああああああああああ</p>
+                <p>あああああああああああああ</p>
+            </div>
+            <div class="right-column">
+                <div id="field">
+                    <div id="home" class="top-image-area">
+                        <a href="index.php"><img class="logo" src="siteimages/header2.png" alt="マッチングナビロゴ"></a>
+                        <header class="page-header wrapper">
+                            <nav>
+                                <ul class="main-nav">
+                                    <li><a href="index.php" class="btn4">ホーム</a></li>
+                                    <li><a href="search.php" class="btn4">プロフ検索</a></li>
+                                    <li><a href="bulletinInfo.php" class="btn4">掲示板</a></li>
+                                    <li><a href="chatInfo.php" class="btn4">メッセージ</a></li>
+                                    <li><a href="myPage.php" class="btn4">マイページ</a></li>
+                                    <li><a href="contact.html" class="btn4">お問い合わせ</a></li>
+                                </ul>
+                            </nav>
+                        </header>
+                        <div id="pad"></div>
+                        <div class="container">
+                            <a href="./profile.php">
+                                <div class="item">
+                                    <p>プロフィール</p></br><img src="./siteimages/profile.png" width="110" height="110" alt="アイコン">
+                                </div>
+                            </a>
+                            <a href="./loginInfo.php">
+                                <div class="item">
+                                    <p>登録情報</p><br><img src="./siteimages/user.jpg" width="120" height="120" alt="アイコン">
+                                </div>
+                            </a>
+                            <a href="./confirmFootPrint.php">
+                                <div class="item">
+                                    <p>足あと</p><img src="./siteimages/trace.png" width="156" height="156" alt="アイコン">
+                                </div>
+                            </a>
+                            <a href="./confirmGood.php">
+                                <div class="item">
+                                    <p>あなたへのいいね</p></br><img src="./siteimages/good.jpg" width="120" height="120" alt="アイコン">
+                                </div>
+                            </a>
+                            <a href="./confirmLove.php">
+                                <div class="item">
+                                    <p>あなたへのタイプ</p></br><img src="./siteimages/heart.png" width="156" height="120" alt="アイコン">
+                                </div>
+                            </a>
+                            <a href="./confirmMyGood.php">
+                                <div class="item">
+                                    <p>いいねした相手</p></br><img src="./siteimages/putGood.jpg" width="156" height="120" alt="アイコン">
+                                </div>
+                            </a>
+                            <a href="./confirmMyLove.php">
+                                <div class="item">
+                                    <p>タイプした相手</p></br><img src="./siteimages/putLove.jpg" width="156" height="120" alt="アイコン">
+                                </div>
+                            </a>
+                            <a href="./info.php">
+                                <div class="item">
+                                    <p>お知らせ</p></br><img src="./siteimages/info.png" width="130" height="130" alt="アイコン">
+                                </div>
+                            </a>
+                            <a href="./news.php">
+                                <div class="item">
+                                    <p>最新情報</p></br></br><img src="./siteimages/news.png" width="100" height="100" alt="アイコン">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
