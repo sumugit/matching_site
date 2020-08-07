d0b07c542c5d4eb8928b5cb457f2d63c49620ed3
sumugit <diddy2983@yahoo.co.jp>
2020-08-07 16:05:48 +0900

    プロフィール完成2周目

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <!--CSS-->
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="siteimages/matchingNav.png">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
     <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
